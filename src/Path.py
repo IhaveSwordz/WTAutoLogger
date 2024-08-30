6f0afd976192c8c83a429ffd1b766323dd52d573
IhaveSwordz <60492291+IhaveSwordz@users.noreply.github.com>
2024-08-30 13:32:04 -0400

    added Path.py file

---
+++ src/Path.py
@@ -0,0 +1,16 @@
+import sys
+class Path_u:
+    def __init__(self):
+        self.path = ""
+        print(sys.path)
+        if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+            print('running in a PyInstaller bundle')
+            self.path = '\\'.join(sys.path[0].split("\\")[:-1])
+        else:
+            print('running in a normal Python process')
+            self.path = sys.path[0]
+        print(self.path)
+
+
+class Path:
+    path = Path_u().path
