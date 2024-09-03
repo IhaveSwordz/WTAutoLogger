892d40b0f757d43bf9c2c1a5522c8c48e835d868
IhaveSwordz <60492291+IhaveSwordz@users.noreply.github.com>
2024-09-02 20:05:39 -0400

    added needed atlases.vromfs.bin_u files

--- src/Path.py
+++ src/Path.py
@@ -5,7 +5,7 @@ class Path_u:
         print(sys.path)
         if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
             print('running in a PyInstaller bundle')
-            self.path = '\\'.join(sys.path[0].split("\\")[:-1])
+            self.path = '\\'.join(sys.path[0].split("\\")[:-2])
         else:
             print('running in a normal Python process')
             self.path = sys.path[0]
